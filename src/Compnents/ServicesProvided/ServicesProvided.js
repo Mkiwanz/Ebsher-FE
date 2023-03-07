82fb900f8dfde781b9a9ea9ea6e584e8c452bdb0
Mkiwanz <117238114+Mkiwanz@users.noreply.github.com>
2023-03-06 21:14:11 -0600

    style stuff

--- src/Compnents/ServicesProvided/ServicesProvided.js
+++ src/Compnents/ServicesProvided/ServicesProvided.js
@@ -1,4 +1,3 @@
-
 const ServicesProvided = () => {
   return (
     <div className="container-fluid py-5" id="service">
@@ -13,60 +12,55 @@ const ServicesProvided = () => {
           <h1 className="font-secondary display-4">What We Cover</h1>
         </div>
         <div className="row">
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-3 mb-3">
             <div className="product-item mb-2">
               <div className="product-img">
                 <img
                   className="img-fluid"
                   src={require("../../Resources/img/product-1.jpg")}
                 />
-                <a
-                  type="button"
-                  className="btn btn-primary"
-                  data-toggle="modal"
-                  data-target="#exampleModalLong"
-                >
-                  <i className="fa fa-2x fa-plus text-white"></i>
-                </a>
               </div>
               <div className="bg-secondary text-center p-4">
-                <h3 className="m-0">Solar System</h3>
+                <h3 className="m-0">On Grid Solar Systems </h3>
               </div>
             </div>
           </div>
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-3 mb-3">
             <div className="product-item mb-2">
               <div className="product-img">
-                <img className="img-fluid" src={require("../../Resources/img/product-2.jpg")} />
-                <a
-                  type="button"
-                  className="btn btn-primary"
-                  data-toggle="modal"
-                  data-target="#exampleModalLong"
-                >
-                  <i className="fa fa-2x fa-plus text-white"></i>
-                </a>
+                <img
+                  className="img-fluid"
+                  src={require("../../Resources/img/carousel-2.jpg")}
+                />
               </div>
               <div className="bg-secondary text-center p-4">
-                <h3 className="m-0">Wind Turbine</h3>
+                <h3 className="m-0">Off Grid Solar Systems</h3>
               </div>
             </div>
           </div>
-          <div className="col-lg-4 mb-3">
+          <div className="col-lg-3 mb-3">
             <div className="product-item mb-2">
               <div className="product-img">
-                <img className="img-fluid" src={require("../../Resources/img/product-3.jpg")} />
-                <a
-                  type="button"
-                  className="btn btn-primary"
-                  data-toggle="modal"
-                  data-target="#exampleModalLong"
-                >
-                  <i className="fa fa-2x fa-plus text-white"></i>
-                </a>
+                <img
+                  className="img-fluid"
+                  src={require("../../Resources/img/gallery-3.jpg")}
+                />
               </div>
               <div className="bg-secondary text-center p-4">
-                <h3 className="m-0">Wind Generator</h3>
+                <h3 className="m-0">Solar water pumping systems</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 mb-3">
+            <div className="product-item mb-2">
+              <div className="product-img">
+                <img
+                  className="img-fluid"
+                  src={require("../../Resources/img/product-3.jpg")}
+                />
+              </div>
+              <div className="bg-secondary text-center p-4">
+                <h3 className="m-0">Cleaning Services</h3>
               </div>
             </div>
           </div>
